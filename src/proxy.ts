2f28d63194125f44811640525506f85dc31aeac5
devaPOC <devat3204@gmail.com>
2026-09-20 22:28:46 +0530

    refactor: rename middleware to proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export default async function middleware(req: NextRequest) {
+export default async function proxy(req: NextRequest) {
   const requestHeaders = new Headers(req.headers);
   const url = req.nextUrl;
 
